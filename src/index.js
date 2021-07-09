211684a83ccd062fc08f9236fbe054d57db9ba07
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-09 19:15:57 -0400

    Completed simple form component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React, { useState } from "react";
+import { render } from "react-dom";
 // 👉 App contains a more sophisticated form we'll flesh out later
-import App from './components/App'
+import App from "./components/App";
 
 // 👉 First let's build a SimpleForm to add more pets:
 const petsList = [
-  { petName: 'Fido', petType: 'dog' },
-  { petName: 'Tweetie', petType: 'canary' },
-  { petName: 'Goldie', petType: 'fish' },
-]
+  { petName: "Fido", petType: "dog" },
+  { petName: "Tweetie", petType: "canary" },
+  { petName: "Goldie", petType: "fish" },
+];
+
+const initialFormValues = {
+  petName: "",
+  petType: "",
+};
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (event) => {
+    //change code
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value }); // name will either be petName or petValue
+  };
+  const submit = (event) => {
+    event.preventDefault();
+    const newPet = {
+      petName: formValues.petName.trim(),
+      petType: formValues.petType.trim(),
+    };
+
+    setPets([...pets, newPet]);
+    setFormValues(initialFormValues);
+  };
+
+  return (
+    <div className="container">
+      <h1>Simple Form</h1>
+
+      {pets.map((pet, index) => {
+        return (
+          <div key={index}>
+            {pet.petName} is a {pet.petType}
+          </div>
+        );
+      })}
+
+      <form onSubmit={submit}>
+        <input name="petName" type="text" value={formValues.petName} onChange={change} />
+        <input name="petType" type="text" value={formValues.petType} onChange={change} />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 render(
   <>
     <SimpleForm />
     {/* <App /> */}
-  </>
-  , document.querySelector('#root')
-)
+  </>,
+  document.querySelector("#root")
+);
